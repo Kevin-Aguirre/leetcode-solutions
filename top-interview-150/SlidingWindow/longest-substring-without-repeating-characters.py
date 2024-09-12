86046fcdc2888f1e2ccca5cf08a331bc1c218eb9
Kevin Aguirre <kevinaguirre463@gmail.com>
2024-09-12 14:08:46 -0400

    added longest substring w/o repeating characters

--- top-interview-150/SlidingWindow/longest-substring-without-repeating-characters.py
+++ top-interview-150/SlidingWindow/longest-substring-without-repeating-characters.py
@@ -33,10 +33,31 @@ s consists of English letters, digits, symbols and spaces.
 
 
 """
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    """
+    Approach: Intialize left pointer as 0, hold a character set, have a result variable. 
+    Iterate through the string, using it as a right pointer. 
+    
+    if we encounter that the char 
+    of s at index right is in the character set, we have to shorten the window from the left 
+    side until that new character we encountered is removed from the set, as we do this we are 
+    incrementing left pointer. 
+
+    whether or not we found an element that occured in the set, we add the new character to the set,
+    update the maximum length using the values of left and right, and adding 1 because indexing 
+    and inclusiveness and math. 
+    """
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        res = 0 
+        charSet = set() 
+        l = 0
+
+        for r in range(len(s)):
+            while (s[r] in charSet):
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, (r - l) + 1)
+            
         
+        return res 
