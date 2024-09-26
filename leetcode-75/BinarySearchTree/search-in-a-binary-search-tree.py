0f2061c70efb4f637d885afa40e0905a14994eed
Kevin Aguirre <kevinaguirre463@gmail.com>
2024-09-26 19:24:35 -0400

    added search in a binary tree

--- leetcode-75/BinarySearchTree/search-in-a-binary-search-tree.py
+++ leetcode-75/BinarySearchTree/search-in-a-binary-search-tree.py
@@ -30,16 +30,23 @@ root is a binary search tree.
 
 """
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def searchBST(self, root, val):
-        """
-        :type root: TreeNode
-        :type val: int
-        :rtype: TreeNode
-        """
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        curr = root
+        while (curr):
+            if (curr.val == val):
+                return curr
+            elif (val < curr.val):
+                curr = curr.left 
+            else:
+                curr = curr.right 
+        return None
         
