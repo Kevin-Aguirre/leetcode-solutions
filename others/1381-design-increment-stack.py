180ed92ba31997ee3579868c6ec0a7419301ca2b
Kevin Aguirre <kevinaguirre463@gmail.com>
2024-09-29 22:45:21 -0400

    added design increment stack

---
+++ others/1381-design-increment-stack.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/design-a-stack-with-increment-operation/
+# 1381. Design a Stack With Increment Operation
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.arr = [-1] * maxSize
+        self.currSize = 0 
+        self.maxSize = maxSize
+
+    def push(self, x: int) -> None:
+        if (self.currSize != self.maxSize):
+            self.arr[self.currSize] = x
+            self.currSize += 1
+
+    def pop(self) -> int:
+        if (self.currSize == 0):
+            return -1
+        val = self.arr[self.currSize - 1]
+        self.arr[self.currSize - 1] = -1
+        self.currSize -= 1
+        return val
+
+    def increment(self, k: int, val: int) -> None:
+
+        curr = 0 
+        while curr < self.currSize and curr < k:
+            self.arr[curr] += val
+            curr += 1
+
+        
+
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
