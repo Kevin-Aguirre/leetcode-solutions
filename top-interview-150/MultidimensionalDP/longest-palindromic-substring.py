88c453ff6051bc393aa817d8e30a0f4d9b05a0d6
Kevin Aguirre <kevinaguirre463@gmail.com>
2024-09-30 11:05:32 -0400

    added longest palindromic substring

--- top-interview-150/MultidimensionalDP/longest-palindromic-substring.py
+++ top-interview-150/MultidimensionalDP/longest-palindromic-substring.py
@@ -25,10 +25,29 @@ s consist of only digits and English letters.
 
 
 """
-class Solution(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ''
+        resLen = 0 
+
+        for i in range(len(s)):
+            # odd length 
+            l, r = i, i 
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1 > resLen):
+                    resLen = r - l + 1
+                    res = s[l:r+1]
+                l -= 1
+                r += 1
+            
+
+            # even length
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if (r - l + 1 > resLen):
+                    resLen = r - l + 1
+                    res = s[l:r+1]
+                l -= 1
+                r += 1
+    
+        return res
