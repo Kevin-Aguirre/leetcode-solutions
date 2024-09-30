f7a897cfac8dd0069703dc260d526f128fe3fa35
Kevin Aguirre <kevinaguirre463@gmail.com>
2024-09-30 15:00:29 -0400

    added max num of k sum pairs

--- leetcode-75/TwoPointers/max-number-of-k-sum-pairs.py
+++ leetcode-75/TwoPointers/max-number-of-k-sum-pairs.py
@@ -32,11 +32,20 @@ Constraints:
 
 
 """
-class Solution(object):
-    def maxOperations(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        
+class Solution:
+    # runtime O(nlogn); space O(1)
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        l, r = 0, len(nums) - 1
+
+        res = 0
+        while (l < r):
+            if (nums[l] + nums[r] == k):
+                res += 1
+                l += 1
+                r -= 1
+            elif (nums[l] + nums[r] < k):
+                l += 1
+            else:
+                r -= 1
+        return res
