cecf5a721611fe6597372f0907685d76996e39c5
Kevin Aguirre <kevinaguirre463@gmail.com>
2024-10-09 15:01:17 -0400

    added divisor game

---
+++ other/1025-divisor-game.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/divisor-game/solutions/382233/solution-in-python-3-with-detailed-proof/?envType=problem-list-v2&envId=brainteaser
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        return n % 2 == 0 
+        
