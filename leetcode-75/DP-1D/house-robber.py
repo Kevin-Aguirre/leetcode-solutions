01540a463f4286c5d231386817d343595993324f
Kevin Aguirre <kevinaguirre463@gmail.com>
2024-11-11 15:29:58 -0500

    added 198

--- leetcode-75/DP-1D/house-robber.py
+++ leetcode-75/DP-1D/house-robber.py
@@ -29,10 +29,20 @@ Constraints:
 
 
 """
-class Solution(object):
-    def rob(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if (len(nums) == 1): return nums[0] 
+
+        nums[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            nums[i] = max(nums[i] + nums[i-2], nums[i-1])
+        return max(nums[-1], nums[-2])
+
+    def rob(self, nums: List[int]) -> int: 
+        rob1, rob2 = 0, 0
+        for n in nums:
+            tmp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = tmp
+        return rob2
